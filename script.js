16c6109fd39127185a2716107f9c375382fcf7d3
Raquel Maia <raquelmaia860@gmail.com>
2023-01-04 19:59:06 -0300

    alterações

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-  //pegar  a tag img
-  const img = document.querySelector("#profile  img")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
   // substituir a imagem
   if (html.classList.contains("light")) {
-    // se tiver light mode.adIcionar a imagem light
+    // se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/Avatar.light.png")
-    img.setAttribute(
-      "alt",
-      "foto de Raquel Maia, mulher de cabelo preto, com fundo azul"
-    )
+    
   } else {
-    // se tiver sem light mode manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
-    img.setAttribute(
-      "alt",
-      "foto de Raquel Maia, mulher, sorrindo, de blusa azul e cabelo preto"
-    )
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("alt", "./assets/avatar.png")
   }
 }
